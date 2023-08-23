eb83311f59d3132077fa4ebd0d22d19ee39aae9f
azimolab <george@azimolab.io>
2023-08-22 23:56:02 -0300

    Adicionando Ribbon

--- jsx/aftereffects copy.jsx
+++ jsx/aftereffects copy.jsx
@@ -185,59 +185,9 @@ function findItemByName(name) {
 }
 
 function duplicatePrecompToOutput(values) {
-  var colorMapping = {
-    Teal: {
-      BG: [0x00 / 255, 0x3e / 255, 0x44 / 255], // 003E44
-      Color01: [0x00 / 255, 0x4f / 255, 0x4d / 255], // 004F4D
-      Color02: [0x21 / 255, 0xaf / 255, 0x90 / 255], // 21AF90
-    },
-    Red: {
-      BG: [0x60 / 255, 0x00 / 255, 0x1d / 255], // 60001D
-      Color01: [0x8c / 255, 0x00 / 255, 0x21 / 255], // 8C0021
-      Color02: [0xe4 / 255, 0x00 / 255, 0x15 / 255], // E40015
-    },
-    Purple: {
-      BG: [0x29 / 255, 0x09 / 255, 0x68 / 255], // 290968
-      Color01: [0x3d / 255, 0x15 / 255, 0x7f / 255], // 3D157F
-      Color02: [0x87 / 255, 0x32 / 255, 0xe1 / 255], // 8732E1
-    },
-    Orange: {
-      BG: [0x91 / 255, 0x22 / 255, 0x10 / 255], // 912210
-      Color01: [0xd8 / 255, 0x35 / 255, 0x00 / 255], // D83500
-      Color02: [0xff / 255, 0x71 / 255, 0x00 / 255], // FF7100
-    },
-    Green: {
-      BG: [0x17 / 255, 0x49 / 255, 0x1d / 255], // 17491D
-      Color01: [0x27 / 255, 0x62 / 255, 0x30 / 255], // 276230
-      Color02: [0xbd / 255, 0xd3 / 255, 0x20 / 255], // BDD320
-    },
-    Furchsia: {
-      BG: [0x68 / 255, 0x04 / 255, 0x51 / 255], // 680451
-      Color01: [0x97 / 255, 0x00 / 255, 0x77 / 255], // 970077
-      Color02: [0xe8 / 255, 0x46 / 255, 0xd6 / 255], // E846D6
-    },
-    Blue: {
-      BG: [0x08 / 255, 0x00 / 255, 0x72 / 255], // 080072
-      Color01: [0x1f / 255, 0x0a / 255, 0xa5 / 255], // 1F0AA5
-      Color02: [0x15 / 255, 0x8e / 255, 0xff / 255], // 158EFF
-    },
-    Aqua: {
-      BG: [0x0c / 255, 0x41 / 255, 0x59 / 255], // 0C4159
-      Color01: [0x0a / 255, 0x5c / 255, 0x83 / 255], // 0A5C83
-      Color02: [0x3a / 255, 0xb5 / 255, 0xe2 / 255], // 3AB5E2
-    },
-    Salmon: {
-      BG: [0x81 / 255, 0x1f / 255, 0x2c / 255], // 811F2C
-      Color01: [0xc1 / 255, 0x2d / 255, 0x3c / 255], // C12D3C
-      Color02: [0xff / 255, 0x56 / 255, 0x5a / 255], // FF565A
-    },
-  };
-
   var precompName = values.style + " " + values.colorScheme;
   var customDuration = parseFloat(values.duration);
-
   var path = values.path + "/library/";
-
   var Name = "Template_Library";
   var resolution = parseFloat(values.resolution);
   var roi = values.roi;
@@ -245,8 +195,6 @@ function duplicatePrecompToOutput(values) {
   var aspectRatio = values.aspectRatio.replace(":", "X"); // Aqui está a mudança
 
   importAEPFile(path, Name);
-
-  //alert(values.resolution);
 
   var ConocoFolder = findItemByName("Conoco");
   if (!ConocoFolder || !(ConocoFolder instanceof FolderItem)) {
@@ -259,13 +207,14 @@ function duplicatePrecompToOutput(values) {
     alert("Pasta 'Library' não encontrada!");
     return;
   }
-  // Procura a subpasta correta dentro da pasta "Library" com base em values.style
+
   var styleFolder;
   switch (values.style) {
     case "Solid Mark Motif":
       precompName = values.style + " " + values.colorScheme;
       styleFolder = findItemByName("Solid Mark Motif", libraryFolder);
       break;
+
     case "Linear Mark Motif":
       var firstWord = values.colorScheme.split(" ")[0];
       precompName = values.style + " " + firstWord;
@@ -282,6 +231,7 @@ function duplicatePrecompToOutput(values) {
       }
 
       break;
+
     case "3D":
       styleFolder = findItemByName("3D", libraryFolder);
 
@@ -292,21 +242,23 @@ function duplicatePrecompToOutput(values) {
       precompName = values.style + " " + values.duration + " " + aspectRatio;
       //alert(precompName);
       break;
+
     case "Ribbon":
       styleFolder = findItemByName("Ribbon", libraryFolder);
 
-      var folder10 = findItemByName(values.duration, styleFolder);
+      var folder11 = findItemByName(values.duration, styleFolder);
       //alert(values.duration);
 
-      styleFolder = folder10;
+      styleFolder = folder11;
       precompName = values.style + " " + values.duration + " " + aspectRatio;
 
-      alert("Ribbon");
+      // alert("Ribbon");
 
       break;
   }
 
   // alert(precompName);
+  // alert(styleFolder);
 
   if (!styleFolder || !(styleFolder instanceof FolderItem)) {
     alert("Subpasta correspondente a '" + values.style + "' não encontrada na pasta 'Library'!");
@@ -353,6 +305,14 @@ function duplicatePrecompToOutput(values) {
       // A região de interesse começa no canto superior esquerdo após reposicionar as camadas
     }
   }
+
+  var colorMapping = {
+    Teal: {
+      BG: [0x00 / 255, 0x3e / 255, 0x44 / 255], // 003E44
+      Color01: [0x00 / 255, 0x4f / 255, 0x4d / 255], // 004F4D
+      Color02: [0x21 / 255, 0xaf / 255, 0x90 / 255], // 21AF90
+    },
+  };
 
   switch (values.style) {
     case "Solid Mark Motif":
